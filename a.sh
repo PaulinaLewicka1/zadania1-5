fc34edd3e1c0e55b89fdf0cb371c330f4515ef9f
Paulina Lewicka <93772466+PaulinaLewicka1@users.noreply.github.com>
2023-01-30 23:59:35 +0100

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,14 +1,31 @@
-if [ -n  "$a" ]; then
-        if [ -n "$s" ]; then
-                echo "$list" | awk '{for (i=0;i<NF;i++) {mb=$(i+1)*2^(i-8); sum+=mb; printf "%d %d %.2f\n", i, $(i+1), mb;}} END {printf "%.2f\n", sum;}'
-        else
-                echo "$list" | awk '{for (i=0;i<NF;i++) {mb=$(i+1)*2^(i-8); printf "%d %d %.2f\n", i, $(i+1), mb;}}'
-        fi
-elif [ -n "$1" ]; then
-        if [ -n "$s" ]; then
-                echo "$list" | awk -v order="$1" '{for (i=0;i<NF;i++) {mb=$(i+1)*2^(i-8); sum+=mb;} printf "%d %d %.2f\n", order, $(order+1), $(order+1)*2^(order-8);} END {printf "%.2f\n", sum;}'
-        else
-                echo "$list" | awk -v nr="$1" '{printf "%d %d %.2f\n", order, $(order+1), $(order+1)*2^(order-8);}'
-        fi
+n=3
+file=
+while getopts ":f:n:" opt
+do
+        case $opt in
+                f) file="$OPTARG";;
+                n) n="$OPTARG";;
+                *) echo "opcja nieprawidlowa: -$OPTARG"
+                        exit 1;;
+        esac
+done
+
+shift $((OPTIND-1))
+
+if [ -n "$file" ]; then
+        pslist=$(sed 1d "$file");
+else
+        pslist=$(ps -eo user,vsz,rsz,pcpu,comm | sed 1d);
 fi
-               
+
+echo "CPU hogs:"
+echo "$pslist" | sort -rn -k4 | head -n"$n" | tr -s ' ' | cut -d' ' -f 1,4,5 | awk'{printf "%10s %16s %16s\n", $1, $2, $3;}'
+
+echo
+echo "RES hogs:"
+echo "$pslist" | sort -rn -k3 | head -n"$n" | tr -s ' ' | cut -d' ' -f 1,3,5 | awk'{printf "%10s %16s %16s\n", $1, $2, $3;}'
+
+echo
+echo "VIRT hogs:"
+echo "$pslist" | sort -rn -k2 | head -n"$n" | tr -s ' ' | cut -d' ' -f 1,2,5 | awk'{printf "%10s %16s %16s\n", $1, $2, $3;}'
+
